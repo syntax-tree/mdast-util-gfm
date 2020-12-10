c287df8825a4d388e028b34da3a36de19807a41f
Titus Wormer <tituswormer@gmail.com>
2020-12-10 10:40:03 +0100

    Refactor to use `mdast-util-to-markdown`

--- to-markdown.js
+++ to-markdown.js
@@ -2,28 +2,21 @@ var autolinkLiteral = require('mdast-util-gfm-autolink-literal/to-markdown')
 var strikethrough = require('mdast-util-gfm-strikethrough/to-markdown')
 var table = require('mdast-util-gfm-table/to-markdown')
 var taskListItem = require('mdast-util-gfm-task-list-item/to-markdown')
+var configure = require('mdast-util-to-markdown/lib/configure')
 
 module.exports = toMarkdown
 
 function toMarkdown(options) {
-  var extensions = [
-    autolinkLiteral,
-    strikethrough,
-    table(options),
-    taskListItem
-  ]
-  var length = extensions.length
-  var index = -1
-  var extension
-  var unsafe = []
-  var handlers = {}
+  var config = configure(
+    {handlers: {}, join: [], unsafe: [], options: {}},
+    {
+      extensions: [autolinkLiteral, strikethrough, table(options), taskListItem]
+    }
+  )
 
-  while (++index < length) {
-    extension = extensions[index]
-    // istanbul ignore next - unsafe always exists, for now.
-    unsafe = unsafe.concat(extension.unsafe || [])
-    handlers = Object.assign(handlers, extension.handlers || {})
-  }
-
-  return {unsafe: unsafe, handlers: handlers}
+  return Object.assign(config.options, {
+    handlers: config.handlers,
+    join: config.join,
+    unsafe: config.unsafe
+  })
 }

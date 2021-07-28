e1f058012e8905828c15e44bf2f2103bbd3c8698
Titus Wormer <tituswormer@gmail.com>
2021-07-28 14:00:30 +0200

    Update `xo`

--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
-import fs from 'fs'
-import path from 'path'
+import fs from 'node:fs'
+import path from 'node:path'
 import test from 'tape'
 import Slugger from 'github-slugger'
 import {toHast} from 'mdast-util-to-hast'

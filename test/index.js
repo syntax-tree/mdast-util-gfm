ea37304e8a297abf8f83b6e5bc1b04ed16358728
Titus Wormer <tituswormer@gmail.com>
2023-07-10 17:56:47 +0200

    Refactor code-style

--- test/index.js
+++ test/index.js
@@ -2,36 +2,32 @@ import assert from 'node:assert/strict'
 import fs from 'node:fs/promises'
 import test from 'node:test'
 import {slug} from 'github-slugger'
-import {toHast} from 'mdast-util-to-hast'
 import {toHtml} from 'hast-util-to-html'
-import {fromMarkdown} from 'mdast-util-from-markdown'
-import {toMarkdown} from 'mdast-util-to-markdown'
 import {gfm} from 'micromark-extension-gfm'
+import {fromMarkdown} from 'mdast-util-from-markdown'
+import {toHast} from 'mdast-util-to-hast'
+import {toMarkdown} from 'mdast-util-to-markdown'
 import {gfmFromMarkdown, gfmToMarkdown} from '../index.js'
-import * as mod from '../index.js'
 import {spec} from './spec.js'
 
-test('core', () => {
-  assert.deepEqual(
-    Object.keys(mod).sort(),
-    ['gfmFromMarkdown', 'gfmToMarkdown'],
-    'should expose the public api'
-  )
+test('core', async function (t) {
+  await t.test('should expose the public api', async function () {
+    assert.deepEqual(Object.keys(await import('../index.js')).sort(), [
+      'gfmFromMarkdown',
+      'gfmToMarkdown'
+    ])
+  })
 })
 
-test('markdown -> mdast', async () => {
-  const files = spec.filter(
-    (example) => !/disallowed raw html/i.test(example.category)
-  )
+test('markdown -> mdast', async function (t) {
+  const files = spec.filter(function (example) {
+    return !/disallowed raw html/i.test(example.category)
+  })
   let index = -1
 
   while (++index < files.length) {
     const example = files[index]
     const name = index + '-' + slug(example.category)
-    const mdast = fromMarkdown(example.input, {
-      extensions: [gfm()],
-      mdastExtensions: [gfmFromMarkdown()]
-    })
 
     if (
       name === '24-autolinks' ||
@@ -42,6 +38,11 @@ test('markdown -> mdast', async () => {
       continue
     }
 
+    const mdast = fromMarkdown(example.input, {
+      extensions: [gfm()],
+      mdastExtensions: [gfmFromMarkdown()]
+    })
+
     const hast = toHast(mdast, {allowDangerousHtml: true})
     assert(hast, 'expected node')
     const actualHtml = toHtml(hast, {
@@ -51,36 +52,46 @@ test('markdown -> mdast', async () => {
     })
 
     /** @type {string} */
-    let expectedHtml
-    /** @type {string} */
     let expectedMarkdown
-    const expectedUrl = new URL('fixture/' + name + '.html', import.meta.url)
-    const inputUrl = new URL('fixture/' + name + '.md', import.meta.url)
 
-    try {
-      expectedHtml = String(await fs.readFile(expectedUrl))
-    } catch {
-      expectedHtml = example.output.slice(0, -1)
-    }
+    const actualMarkdown = toMarkdown(mdast, {
+      extensions: [gfmToMarkdown()]
+    })
 
-    const actualMarkdown = toMarkdown(mdast, {extensions: [gfmToMarkdown()]})
+    await t.test(
+      example.category + ' (' + index + ') -> html',
+      async function () {
+        const expectedUrl = new URL(
+          'fixture/' + name + '.html',
+          import.meta.url
+        )
 
-    try {
-      expectedMarkdown = String(await fs.readFile(inputUrl))
-    } catch {
-      expectedMarkdown = actualMarkdown
-      await fs.writeFile(inputUrl, expectedMarkdown)
-    }
+        /** @type {string} */
+        let expectedHtml
+        try {
+          expectedHtml = String(await fs.readFile(expectedUrl))
+        } catch {
+          expectedHtml = example.output.slice(0, -1)
+        }
 
-    assert.equal(
-      actualHtml,
-      expectedHtml,
-      example.category + ' (' + index + ') -> html'
+        assert.equal(actualHtml, expectedHtml)
+      }
     )
-    assert.equal(
-      actualMarkdown,
-      expectedMarkdown,
-      example.category + ' (' + index + ') -> md'
+
+    await t.test(
+      example.category + ' (' + index + ') -> md',
+      async function () {
+        const inputUrl = new URL('fixture/' + name + '.md', import.meta.url)
+
+        try {
+          expectedMarkdown = String(await fs.readFile(inputUrl))
+        } catch {
+          expectedMarkdown = actualMarkdown
+          await fs.writeFile(inputUrl, expectedMarkdown)
+        }
+
+        assert.equal(actualMarkdown, expectedMarkdown)
+      }
     )
   }
 })
